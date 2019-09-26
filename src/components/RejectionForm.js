cc1073bf20feaf694139f9675e87fafad67c64f6
Rex Petersen <rex_p_petersen@homedepot.com>
2019-09-26 13:31:52 -0500

    fix minor spacing issues

--- src/components/RejectionForm.js
+++ src/components/RejectionForm.js
@@ -4,36 +4,36 @@ import setter, { checkBoxSetter } from '../util/eventSetter';
 
 function RejectionForm ({ score, createQuestion }) {
 
-			const [currentAskee, setCurrentAskee] = useState('');
-			const [currentQuestion, setCurrentQuestion] = useState('');
-			const [currentlyRejected, setCurrentRejected] = useState('accepted');
+	const [currentAskee, setCurrentAskee] = useState('');
+	const [currentQuestion, setCurrentQuestion] = useState('');
+	const [currentlyRejected, setCurrentRejected] = useState('accepted');
 
-	        return (
+	return (
+		<div>
+			<h1 className="score">Score: {score}</h1>
+			<form id='rejectionForm' onSubmit={() => createQuestion({ askee: currentAskee, question: currentQuestion, status: currentlyRejected})}>
 				<div>
-					<h1 className="score">Score: {score}</h1>
-					<form id='rejectionForm' onSubmit={() => createQuestion({ askee: currentAskee, question: currentQuestion, status: currentlyRejected})}>
-						<div>
-							<label>
-								Askee:
-								<input type="text" id="rejectionAskeeTextInput" onChange={setter(setCurrentAskee)} /> 
-							</label> 
-						</div>
-						<div>
-							<label>
-								Question:
-								<input type="text" id="rejectionQuestionTextInput" onChange={setter(setCurrentQuestion)}/>
-							</label>
-							<label>
-								Rejected:
-								<input type="checkbox" id="rejectionCheckboxInput" onChange={checkBoxSetter(setCurrentRejected)(checkStatus)}/>
-							</label>
-						</div>
-						<div>
-							<button>Submit</button>
-						</div>
-					</form>
+					<label>
+						Askee:
+						<input type="text" id="rejectionAskeeTextInput" onChange={setter(setCurrentAskee)} /> 
+					</label> 
 				</div>
-			);
+				<div>
+					<label>
+						Question:
+						<input type="text" id="rejectionQuestionTextInput" onChange={setter(setCurrentQuestion)}/>
+					</label>
+					<label>
+						Rejected:
+						<input type="checkbox" id="rejectionCheckboxInput" onChange={checkBoxSetter(setCurrentRejected)(checkStatus)}/>
+					</label>
+				</div>
+				<div>
+					<button>Submit</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 const checkStatus = (checked) => (checked) ? 'rejected' : 'accepted' ; 
