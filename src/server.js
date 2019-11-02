572abda44d6fd7c89d87b317ca04cbe96a7a458d
Rex Petersen <rex_p_petersen@homedepot.com>
2019-11-02 15:30:35 -0500

    add conncection to mongodb

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const mongoose = require('mongoose');
+
+const dbRoute =
+  'mongodb+srv://dev:jsi43k1xbWITjAsw@rejection-fe1ny.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(dbRoute, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+db.once('open', () => console.log('connected to the database'));
+
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', (req, res) => {
