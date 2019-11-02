fb3e7ee02d38e7d454eef2729d0df325547ba787
Rex Petersen <rex_p_petersen@homedepot.com>
2019-11-02 14:47:52 -0500

    add basic back end logic

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.get('/json', (req, res) => {
+  console.log('bad bad cat');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
